971d932935094aaba5ef2d26cdd6254b1738503e
yuanhao <71644411+Yuanhao-JIANG@users.noreply.github.com>
2021-05-08 16:04:05 +0100

    firstOpenGLGame -- finish VAO related feature

--- src/firstOpenGLGame/main/java/jade/LevelEditorScene.java
+++ src/firstOpenGLGame/main/java/jade/LevelEditorScene.java
@@ -2,41 +2,45 @@ package firstOpenGLGame.main.java.jade;
 
 import firstOpenGLGame.main.java.util.ShaderUtils;
 
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
+import static firstOpenGLGame.main.java.util.VAOUtil.createVAO;
+import static firstOpenGLGame.main.java.util.VAOUtil.draw;
+import static org.lwjgl.opengl.GL20.glUseProgram;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
 
 public class LevelEditorScene extends Scene{
     private boolean changeScene = false;
-    private float[] vertexArray = {
+    private final float[] vertexArray = {
             //position             //color
              0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f, 1.0f, //bottom right
-            -0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f, 1.0f, //top left
-             0.5f,  0.5f, 0.0f,     1.0f, 0.0f, 0.0f, 1.0f, //top right
-            -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f, 1.0f //bottom left
+            -0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f, 1.0f, //top left
+             0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f, 1.0f, //top right
+            -0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f //bottom left
     };
-    private int[] elementArray = {
+    private final int[] elementArray = {
             2, 1, 0, //top right triangle
             0, 1, 3 //bottom left triangle
     };
+    int program;
+    int vao;
 
     public LevelEditorScene() {}
 
     @Override
     public void init() {
-        int programID = ShaderUtils.load(
+        program = ShaderUtils.load(
                 "src/firstOpenGLGame/assets/shaders/vertex.glsl",
                 "src/firstOpenGLGame/assets/shaders/fragment.glsl");
+        vao = createVAO(vertexArray, elementArray);
     }
 
     @Override
     public void update(float dt) {
-        if (!changeScene && Input.isKeyDown(GLFW_KEY_SPACE)) {
-            System.out.println("hi");
-            System.out.println("now in levelEditorScene");
-            changeScene = true;
-        } else if (changeScene) {
-            Window.changeScene(1);
-        }
+        glUseProgram(program);
+        glBindVertexArray(vao);
+        draw(6);
 
+        glUseProgram(0);
+        glBindVertexArray(0);
     }
 
 
--- src/firstOpenGLGame/main/java/util/VAOUtil.java
+++ src/firstOpenGLGame/main/java/util/VAOUtil.java
@@ -1,4 +1,38 @@
 package firstOpenGLGame.main.java.util;
 
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+
 public class VAOUtil {
+    public static int createVAO(float[] vertexArray, int[] indices) {
+        int vao = glGenVertexArrays();
+        int vbo = glGenBuffers(), ebo = glGenBuffers();
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertexArray, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 7 * Float.BYTES, 0);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 4, GL_FLOAT, false, 7 * Float.BYTES,
+                3 * Float.BYTES);
+        glEnableVertexAttribArray(1);
+
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
+        return vao;
+    }
+
+    public static void draw(int count) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, 0);
+    }
 }
